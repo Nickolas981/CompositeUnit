5f3cf95d915665f5715dd478652f1284b4b75339
n.humeniuk <Rd5676dD>
2019-06-13 10:11:04 +0300

    Added binding flag to simple composite unit

--- compositeunit2/src/main/java/com/example/compositeunit2/base/SimpleCompositeUnit.kt
+++ compositeunit2/src/main/java/com/example/compositeunit2/base/SimpleCompositeUnit.kt
@@ -1,6 +1,8 @@
 package com.example.compositeunit2.base
 
-import com.example.compositeunit2.base.CompositeUnit
-
-class SimpleCompositeUnit(override val clazz: Class<*>, override val layoutId: Int, override val spanSize: Int = 1) :
-    CompositeUnit
+class SimpleCompositeUnit(
+    override val clazz: Class<*>,
+    override val layoutId: Int,
+    override val spanSize: Int = 1,
+    override val binding: Boolean
+) : CompositeUnit
