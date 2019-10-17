38f16fb17ec7b32c22a1e2656335c74c262d0741
n.humeniuk <Rd5676dD>
2019-10-17 09:36:20 +0300

    Code clean up

--- compositeunit2/src/main/java/com/example/compositeunit2/base/CompositeBuilder.kt
+++ compositeunit2/src/main/java/com/example/compositeunit2/base/CompositeBuilder.kt
@@ -1,18 +1,8 @@
 package com.example.compositeunit2.base
 
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.asynclayoutinflater.view.AsyncLayoutInflater
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
-import androidx.recyclerview.widget.RecyclerView
 import com.example.compositeunit2.adapter.*
 import com.example.compositeunit2.models.CompositeUnitData
-import com.example.compositeunit2.utils.mapOf
-import java.lang.ref.WeakReference
-import java.util.*
 
 class CompositeBuilder {
     private val units: MutableList<CompositeUnit> = mutableListOf()
@@ -119,139 +109,6 @@ class CompositeBuilder {
                     cud.actionMap[viewType]?.let {
                         it(holder.itemView, getItem(position)!!)
                     }
-                }
-            }
-
-            override fun getSpanSize(viewType: Int): Int {
-                return cud.spanSizeMap[viewType] ?: 1
-            }
-        }
-    }
-
-    fun buildPaged(
-        extraCU: CompositeUnit,
-        extraItem: Any,
-        context: Context,
-        recyclerView: WeakReference<RecyclerView>
-    ): PagedWithExtraRow<Any> {
-        val cud = CompositeUnitData.from(units)
-
-        return object : PagedWithExtraRow<Any>(
-            cud.typesMap, cud.handlerMap, buildCompare(), extraCU, extraItem
-        ) {
-
-            private val asyncLayoutInflater = AsyncLayoutInflater(context)
-            private val cachedViews: Map<Int, Stack<View>> = mapOf(cud.layoutMap.keys) { Stack<View>() }
-
-            init {
-                cud.preloadedViewHoldersSizeMap.toList().filter { it.second != 0 }.forEach {
-                    for (i in 0..it.second) {
-                        asyncLayoutInflater.inflate(cud.layoutMap[it.first] ?: continue, recyclerView.get()) { view, _, _ ->
-                            cachedViews[it.first]?.push(view)
-                        }
-                    }
-                }
-            }
-
-            private fun getView(parent: ViewGroup, viewType: Int): View {
-                val stack = cachedViews[viewType]
-                return if (stack.isNullOrEmpty()) {
-                    LayoutInflater.from(parent.context).inflate(cud.layoutMap[viewType]!!, parent, false)
-                } else {
-                    stack.pop()
-                }
-            }
-
-
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleViewHolder {
-                return if (viewType == EXTRA_ROW_TYPE) {
-                    val binding: ViewDataBinding = DataBindingUtil.inflate(
-                        LayoutInflater.from(parent.context), extraCU.layoutId, parent, false
-                    )
-                    SimpleDataBindingViewHolder(
-                        binding
-                    )
-                } else {
-                    val simpleViewHolder = if (cud.bindingMap[viewType] == true) {
-                        SimpleDataBindingViewHolder(DataBindingUtil.bind(getView(parent, viewType))!!)
-                    } else {
-                        SimpleViewHolder(
-                            getView(parent, viewType)
-                        )
-                    }
-                    cud.createActionMap[viewType]?.let { it(simpleViewHolder.itemView) }
-                    simpleViewHolder
-                }
-            }
-
-            override fun onBindViewHolder(holder: SimpleViewHolder, position: Int) {
-                if (position == itemCount - 1) {
-                    holder.bind(extraItem, extraCU.handler)
-                } else {
-                    val viewType = getItemViewType(position)
-                    if (cud.bindingMap[viewType] == true) {
-                        super.onBindViewHolder(holder, position)
-                    }
-                    cud.actionMap[viewType]?.let {
-                        it(holder.itemView, getItem(position)!!)
-                    }
-                }
-            }
-
-            override fun getSpanSize(viewType: Int): Int {
-                return cud.spanSizeMap[viewType] ?: 1
-            }
-        }
-    }
-
-    fun buildPaged(context: Context): MultiTypedPagedDataBindingAdapter<Any> {
-        val cud = CompositeUnitData.from(units)
-
-        return object : MultiTypedPagedDataBindingAdapter<Any>(
-            cud.typesMap, cud.handlerMap, buildCompare()
-        ) {
-
-            private val asyncLayoutInflater = AsyncLayoutInflater(context)
-            private val cachedViews: Map<Int, Stack<View>> = mapOf(cud.layoutMap.keys) { Stack<View>() }
-
-            init {
-                cud.preloadedViewHoldersSizeMap.toList().filter { it.second != 0 }.forEach {
-                    for (i in 0..it.second) {
-                        asyncLayoutInflater.inflate(cud.layoutMap[it.first] ?: continue, null) { view, _, _ ->
-                            cachedViews[it.first]?.push(view)
-                        }
-                    }
-                }
-            }
-
-            private fun getView(parent: ViewGroup, viewType: Int): View {
-                val stack = cachedViews[viewType]
-                return if (stack.isNullOrEmpty()) {
-                    LayoutInflater.from(parent.context).inflate(cud.layoutMap[viewType]!!, parent, false)
-                } else {
-                    stack.pop()
-                }
-            }
-
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleViewHolder {
-                val simpleViewHolder = if (cud.bindingMap[viewType] == true) {
-                    SimpleDataBindingViewHolder(DataBindingUtil.bind(getView(parent, viewType))!!)
-                } else {
-                    SimpleViewHolder(
-                        getView(parent, viewType)
-                    )
-                }
-                cud.createActionMap[viewType]?.let { it(simpleViewHolder.itemView) }
-                return simpleViewHolder
-            }
-
-            override fun onBindViewHolder(holder: SimpleViewHolder, position: Int) {
-                val viewType = getItemViewType(position)
-                if (cud.bindingMap[viewType] == true) {
-                    super.onBindViewHolder(holder, position)
-                }
-                cud.actionMap[viewType]?.let {
-                    it(holder.itemView, getItem(position)!!)
                 }
             }
 
--- compositeunit2/src/main/java/com/example/compositeunit2/models/CompositeUnitData.kt
+++ compositeunit2/src/main/java/com/example/compositeunit2/models/CompositeUnitData.kt
@@ -7,50 +7,37 @@ import com.example.compositeunit2.base.CompositeUnit
 
 class CompositeUnitData(
     val typesMap: MutableMap<Class<*>, Int>,
-    val layoutMap: MutableMap<Int, Int>,
     val handlerMap: MutableMap<Int, Any>,
     val bindingMap: MutableMap<Int, Boolean>,
     val actionMap: MutableMap<Int, (View, Any) -> Unit>,
-    val createActionMap: MutableMap<Int, (View) -> Unit>,
     val spanSizeMap: MutableMap<Int, Int>,
-    val preloadedViewHoldersSizeMap: MutableMap<Int, Int>,
     val getViewHolderMap: MutableMap<Int, (ViewGroup, Boolean) -> SimpleViewHolder>
 ) {
     companion object {
         fun from(units: List<CompositeUnit>): CompositeUnitData {
             val typesMap: MutableMap<Class<*>, Int> = mutableMapOf()
-            val layoutMap: MutableMap<Int, Int> = mutableMapOf()
             val handlerMap: MutableMap<Int, Any> = mutableMapOf()
             val bindingMap: MutableMap<Int, Boolean> = mutableMapOf()
             val actionMap: MutableMap<Int, (View, Any) -> Unit> = mutableMapOf()
-            val createActionMap: MutableMap<Int, (View) -> Unit> = mutableMapOf()
             val spanSizeMap: MutableMap<Int, Int> = mutableMapOf()
-            val preloadedViewHoldersSizeMap: MutableMap<Int, Int> = mutableMapOf()
             val getViewHolderMap: MutableMap<Int, (ViewGroup, Boolean) -> SimpleViewHolder> =
                 mutableMapOf()
 
             units.forEachIndexed { index, unit ->
                 typesMap[unit.clazz] = index
-                layoutMap[index] = unit.layoutId
                 unit.handler?.let { handlerMap[index] = it }
                 bindingMap[index] = unit.binding
                 unit.action?.let { actionMap[index] = it }
-                unit.createAction?.let { createActionMap[index] = it }
                 spanSizeMap[index] = unit.spanSize
-                unit.createAction?.let { createActionMap[index] = it }
-                preloadedViewHoldersSizeMap[index] = unit.preloadedViewHoldersSize
                 getViewHolderMap[index] = unit::getViewHolder
             }
 
             return CompositeUnitData(
                 typesMap,
-                layoutMap,
                 handlerMap,
                 bindingMap,
                 actionMap,
-                createActionMap,
                 spanSizeMap,
-                preloadedViewHoldersSizeMap,
                 getViewHolderMap
             )
         }
