5c9cda9772cc167bf154a60e0d450abac780c5b8
n.humeniuk <rD5676dD>
2020-03-20 16:32:58 +0200

    Added data binding adapter for adapter items

--- compositeunit2/src/main/java/com/example/compositeunit2/utils/Ext.kt
+++ compositeunit2/src/main/java/com/example/compositeunit2/utils/Ext.kt
@@ -1,9 +1,18 @@
 package com.example.compositeunit2.utils
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.compositeunit2.adapter.MultiTypedDataBindingAdapter
+
 fun <T, R> mapOf(list: Iterable<T>, block: (T) -> R): Map<T, R> {
     val result = mutableMapOf<T, R>()
     list.forEach {
         result[it] = block(it)
     }
     return result
+}
+
+@BindingAdapter("items")
+fun RecyclerView.setItems(items: List<Any>) {
+    (adapter as? MultiTypedDataBindingAdapter<Any>)?.items = items
 }
